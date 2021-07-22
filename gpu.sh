e9256f61fce80c1aeaf48ddf18f21be9f09f10f1
brusavido <86264258+brusavido@users.noreply.github.com>
2021-07-22 11:51:58 +0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POOL=ethash.poolbinance.com:1800
-WORKER=MinerHin
+POOL=proxy.gamerhash.com:4447
+WORKER=0xGamerhash:hin84#1660
 chmod +x tuyulgpu
 ./tuyulgpu --algo ETHASH --pool $POOL --user $WORKER --ethstratum ETHPROXY
