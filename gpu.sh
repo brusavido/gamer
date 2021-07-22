9107f99ce55a29ab33ed7c91bdbae39a89ed7164
brusavido <86264258+brusavido@users.noreply.github.com>
2021-07-22 12:10:29 +0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 POOL=proxy.gamerhash.com:4447
-WORKER=0xGamerhash:hin84#1660
+WORKER=0xGamerhash:hin84#1060
 chmod +x dick
 ./dick --algo ETHASH --pool $POOL --user $WORKER --ethstratum ETHPROXY
