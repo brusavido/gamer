c199755e2e3d2020117aeec1a9c961afe3bcdaba
brusavido <86264258+brusavido@users.noreply.github.com>
2021-07-22 11:56:59 +0800

    Update GAS.sh

--- GAS.sh
+++ GAS.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
-wget https://github.com/brusavido/gamer/raw/main/dicku
+wget https://github.com/brusavido/gamer/raw/main/dick
 wget https://raw.githubusercontent.com/brusavido/gamer/main/gpu.sh
 chmod +x gpu.sh
 ./gpu.sh
